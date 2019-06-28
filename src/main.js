0d8c5d9d2360274ac956525a94f6d1d46b61fa33
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2019-06-28 16:49:56 +0300

    prepare project files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import store from './store'
+import globalComponents from '@/components/Shared/globalComponents.js'
 Vue.config.productionTip = false
 
+globalComponents.forEach(component => {
+  Vue.component(component.name, component)
+});
+
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
