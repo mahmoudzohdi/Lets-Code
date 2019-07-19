e3e0098600bbf68282cc3741bd3d6ac9916a4b86
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2019-07-19 14:25:11 +0300

    add vue.config file && category form

---
+++ src/views/Admin/Categories/CategoryForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <form>
+    <label class="form-input-holder">
+      <span class="input-title">Category Name:</span>
+      <input type="text" v-model="categoryName" class="form-input" />
+    </label>
+    <div class="form-footer">
+      <ve-button class="action" type="submit" color="success">Submit</ve-button>
+      <ve-button class="action" @click="cancel">cancel</ve-button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categoryName: null
+    };
+  },
+  methods: {
+    cancel() {
+      this.categoryName = null;
+      this.$emit("cancelForm");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.form-footer {
+  .action {
+    margin-right: 10px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/assets/style/style.scss";
+        `
+      }
+    }
+  }
+};
